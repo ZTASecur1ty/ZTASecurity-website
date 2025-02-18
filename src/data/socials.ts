f81f46162fd89cfbebfdd7cf4f64634c868575f5
Omar2535 <omar@9tsai.ca>
2025-02-17 21:37:50 -0500

    Initial commit

---
+++ src/data/socials.ts
@@ -0,0 +1,13 @@
+export const tag = "@ztasecurity";
+export const author = "ZTASecurity";
+
+export const defaultMeta = {
+  title: "ZTASecurity",
+  description:
+    "Cybersecurity knowledge for developers",
+  keywords: ["ZTASecurity", "software security"],
+  ogImage: {
+    src: "/banner.png",
+    alt: "ZTASecurity - Home",
+  },
+};
