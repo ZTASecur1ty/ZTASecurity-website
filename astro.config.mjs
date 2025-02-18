a129ce6b2d3fc6f3771a343e6432c0c2eaba772c
Omar2535 <omar@9tsai.ca>
2025-02-17 22:05:07 -0500

    Add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
     service: sharpImageService()
   },
   site: "https://ztasecur1ty.github.io",
+  base: "/ZTASecurity-website",
   vite: {
     plugins: [rawFonts([".ttf", ".woff"])],
     optimizeDeps: {
